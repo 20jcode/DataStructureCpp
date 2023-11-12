8118a794b6c5b2abaf43927a64f48b60590fcdfc
20jcode <20j.code@gmail.com>
2023-11-12 19:07:33 +0900

    save : 작동잘안됨

--- ch4-list/_h3PolyList.md
+++ ch4-list/_h3PolyList.md
@@ -22,5 +22,60 @@ Delete(void) : first를 제거한다.
 다항식 리스트가 된다.
 Chain의 ChainNode로 Term을 가리키게 된다.
 
+---
+
+# _home_03_02
+
+헤드노드가 있는 서큘러 링크드 리스트.
+
+## void Chain<T>::Delete(void)
+
+헤드노드가 last이자 first인 경우 -> 아무 삭제 안해도 됨.
+
+헤드노드 + 실제노드 1개 인 경우 -> 삭제하고 헤드노드 == last
+
+헤드노드 + 실제노드 2개 이상인 경우 -> head->link = head->link->link;
+이때 last는 조작하지 않아도 됨.
+
+
+## void Chain<T>::Add(const T& element)
+
+### 궁금증
+
+?? ChainNode<T>* newNode = new ChainNode<T>(element); 와
+
+그냥 new ChainNode(element) 의 차이는? 
+
+위는 특정 데이터 타입 T를 개체에 생성자로 전달해서 인스턴스화 시키고
+
+아래는 클래스가 특정 타입으로 정의된 경우 사용 가능한 듯?
+
+### 아무튼 구현
+
+1. 헤드노드만 존재하는 경우
+2. 노드 중간에 element가 위치하는 경우
+3. 제일 마지막 값보다 element가 큰 경우
+
+```
+while(p->link != head){ //다음 노드가 head가 아닌 경우에만
+    if(p->data.exp>newNode->data.exp && p->data.exp < newNode->data.exp){
+        // p의 exp > newNode의 exp > p->link의 exp
+        
+    }
+}
+
+```
+
+head -> exp3 인 체인이 존재하는 경우를 생각해보자.
+
+p = 3이고, p->link = head 이므로 while을 돌지 않게 된다.
+
+이때 newNode = 2 인 경우에는 ?
+
+안됨.
+
+다음이 head가 아니고, 다음것보다 크면 삽입, 같으면 더하기?
+
+
 
 
--- ch4-list/_home_03_02CircleHead.cpp
+++ ch4-list/_home_03_02CircleHead.cpp
@@ -50,19 +50,19 @@ public:
 	void InsertBack(const T& element);
 	void displayAll();
 
-	ChainIterator<T> begin() const { return ChainIterator<T>(last); }
+	ChainIterator<T> begin() const { return ChainIterator<T>(head->link); }
 	ChainIterator<T> end() const { return ChainIterator<T>(nullptr); }
 private:
 	ChainNode<T>* last; //last로 수정
-    ChainNode<T>* head;
+    ChainNode<T>* head; //head노드 추가
 };
 
 template<class T> class ChainIterator {
 private:
-	//const Chain<T>* list; //refers to an existing list
+	const Chain<T>* list; //refers to an existing list
 	ChainNode<T>* current; //points to a node in list
 public:
-	//ChainIterator<T>(const Chain<T> &l) :Chain(l), current(l.first) { };
+	ChainIterator<T>(const Chain<T> &l) :Chain<T>(l), current(l.first) { }; //Chain에 대해서 Iterator를 생성
 	ChainIterator() { }
 	ChainIterator(ChainNode<T>* node) :current(node) { }
 	//dereferencing operators
@@ -73,14 +73,14 @@ public:
 	}
 	bool isEmpty() const { return current == NULL; }
 	//increment
-	ChainIterator& operator ++(); //preincrement
-	ChainIterator<T> operator ++(int); //post increment
+	ChainIterator& operator ++(); //preincrement, Iterator를 현재 연산 이후 다음으로 이동한다.
+	ChainIterator<T> operator ++(int); //post increment, Iterator를 다음으로 이동하고 현재 연산을 진행한다.
 	bool NotNull(); //check the current element in list is non-null
 	bool NextNotNull(); //check the next element in list is non-null
-	//T* First( ) { //return a pointer to the first element of list
-	//	if (list->first) return &list->first->data;
-	//	else return 0;
-	//}
+	T* First( ) { //return a pointer to the first element of list
+		if (list->first) return &list->first->data;
+		else return 0;
+	}
 	T* Next();//return a pointer to the next element of list
 };
 
@@ -95,108 +95,192 @@ public:
 	void add(T coef, T exponent);
 	void addAll(Polynomial<T>* poly);
 	void display();
-	/*
+
 	T Evaluate(T&) const;//f(x)에 대하여 x에 대한 값을 구한다
-	polynomial<T> Multiply(Polynomial<T>&); //f(x) * g(x)
+	Polynomial<T> Multiply(Polynomial<T>&); //f(x) * g(x)
 	Polynomial(const Polynomial& p); //copy constructor
 	friend istream& operator>>(istream&, Polynomial&);//polynomial 입력
 	friend ostream& operator<<(ostream&, Polynomial&);//polynomial 출력
-	const Polynomial& operator=(const Polynomial&) const;
-	~Polynomial( );
-	Polynomial operator-(const Polynomial&)const;
-	*/
+	//const Polynomial& operator=(const Polynomial&) const;
+	//~Polynomial( );
+	//Polynomial operator-(const Polynomial&)const;
+
 private:
 	Chain<Term<T> > poly;
 };
 
 template <typename valType>
 inline ostream& operator<< (ostream& os, const Term<valType>& term) {
-
+    os<<"[ coef : "<<term.coef<<", exp : "<<term.exp<<" ]";
+    return os;
 }
 
 template <class T>
 ChainNode<T>::ChainNode(const T& element) {
-
+    data = element;
+    link = nullptr;
 }
 
 template <class T>
 void Chain<T>::Delete(void) //delete the first element after first
 {
+    if(last != head && last->link == head){
+        //노드 내부에 head노드와 1개의 실제 노드가 들어있는 경우
+        head->link = head;
+        last = head;
+
+    } else {
+        //실제 노드가 2개 이상인 경우 : head -> 노드 1 -> 노드 2(last) -> head
+        head->link = head->link->link;
+    }
 
 }
 
 template <class T>
 void Chain<T>::Add(const T& element) //add a new node after first
 {
+    ChainNode<T>* newNode = new ChainNode<T>(element);
+    ChainNode<T>* p = head;
 
+    if(last == head){ //헤드노드만 존재하는 경우
+        head->link = newNode;
+        newNode->link = head;
+        last = newNode;
+    } else { //노드가 1개 이상이고, 중간에 존재하는 경우
+        while(p->link != head){ //다음 노드가 head가 아닌 경우에만
+            if(newNode->data.exp > p->link->data.exp){ // newNode > p->link
+                newNode->link = p->link;
+                p->link = newNode;
+                return;
+            } else if (newNode->data.exp == p->link->data.exp){
+                p->link->data.coef += newNode->data.coef; // 지수가 같으므로 더해준다.
+                return;
+            } else {
+                p = p->link;
+            }
+        } // p == last 이지만, newNode가 더 작은 지수를 가지고 있는 경우
+
+        newNode->link = head;
+        p->link = newNode;
+        last = newNode;
+    }
 }
 
 template <class T>
-void Chain<T>::Invert() {
-	ChainNode<T>* p = first, * q = 0; //q trails p
-	while (p) {
+void Chain<T>::Invert() { //다항식을 뒤집지만, 헤드노드는 유지되어야한다.
+
+    //TODO : 정상작동하는가?
+    if(head->link == head){ //빈노드이면 할 필요가 없음.
+        return;
+    }
+
+	ChainNode<T>* p = head->link, * q = head; //q trails p
+	last = p;
+
+    while (p != head) {
 		ChainNode<T>* r = q; q = p; //r trails q
 		p = p->link; //p moves to next node
 		q->link = r; //link q to preceding node
 	}
-	first = q;
+
 }
 
 template <class T>
-void Chain<T>::Concatenate(Chain<T> b) {
-	if (!first) { first = b.first; return; }
-	if (b.first) {
-		for (ChainNode<T>* p = first; p->link; p = p->link) {
-			p->link = b.first;
-		}
-	}
+void Chain<T>::Concatenate(Chain<T> b) { //연결시키는 것.
+
+	//현재 체인이 없을 경우 b를 가지고 끝낸다.
+
+    if(head->link == head){
+        head->link = b.head->link;
+        last = b.last;
+        return;
+    } else {
+        ChainNode<T>* p = b.head->link;
+
+        if(p->link!= b.head){ //b를 순환하며 Add메서드를 실행한다.
+            Add(p->data);
+            p = p->link;
+        }
+    }
 }
 
 template <class T>
-void Chain<T>::InsertBack(const T& element) {
-	ChainNode<T>* newnode = new ChainNode<T>(element);
-	if (!first) //insert into empty list
-	{
-		first = newnode;
-		return;
-	}
-	ChainNode<T>* curr = first;
-	while (curr->link != NULL) {
-		curr = curr->link;
-	}
-	curr->link = newnode;
+void Chain<T>::InsertBack(const T& element) { //노드의 제일 마지막에 특정 노드를 추가한다.
+    //정렬이 되지 않게 됨. 참고
+
+	ChainNode<T>* newNode = new ChainNode<T>(element);
+	if(head->link == head){
+        head->link = newNode;
+        newNode->link = head;
+        last = newNode;
+        return;
+    } else {
+        newNode->link = head;
+        last->link = newNode;
+        last = newNode;
+        return;
+    }
 }
 
 template <class T>
 void Chain<T>::displayAll() {
 
+    ChainNode<T>* p = head->link;
+
+    cout<<"[ ";
+
+    while(p != head){
+        cout<<p->data;
+        if(p->link != head){
+            cout<<", ";
+        }
+        p = p->link;
+    }
+
+    cout<<" ]"<<endl;
 }
 
 template <class T>
 ChainIterator<T>& ChainIterator<T>::operator ++() //preincrement
 {
+    //next하고 반환한다.
 
+    current = current->link;
+    return current;
 }
 
 template <class T>
 ChainIterator<T> ChainIterator<T>::operator ++(int) //post increment
 {
-
+    //current를 반환하고, next한다.
+    ChainNode<T> temp(*this); //현재 상태를 복사해서 생성
+    current = current->link;
+    return temp;
 }
 
 template <class T>
 bool ChainIterator<T>::NotNull() { //check the current element in list is non-null
-
+    if(current != nullptr){ //data가 0이 아닌경우 null이 아니므로.
+        return true;
+    } else {
+        return false;
+    }
 }
 
 template <class T>
 bool ChainIterator<T>::NextNotNull() { //check the next element in list is non-null
 
+    if(current != nullptr){
+        return true;
+    } else {
+        return false;
+    }
 }
 
 template <class T>
 T* ChainIterator<T>::Next() {//return a pointer to the next element of list
-
+    current = current->link;
+    return &current->data;
 }
 
 template<class T>
@@ -215,6 +299,35 @@ template<class T> void Polynomial<T>::addAll(Polynomial<T>* b) {
 	}
 }
 
+template<class T>
+T Polynomial<T>::Evaluate(T &e) const {
+    ChainIterator<Term<T>> li = poly.begin();
+    T ans;
+
+    while(li.NotNull()){
+        ans += li.coef*pow(e,li.exp);
+        li.Next();
+    }
+
+    return ans;
+}
+template<class T>
+Polynomial<T> Polynomial<T>::Multiply(Polynomial<T> & b) {
+    Term<T> temp;
+	ChainIterator<Term<T>> ai = poly.begin(), bi = b.poly.begin();
+	Polynomial<T> c;
+
+    while(ai.NotNull()){
+        while(bi.NotNull()){
+            c.add(ai.coef*bi.coef,ai.exp*bi.exp);
+            bi.Next();
+        }
+        ai.Next();
+    }
+
+    return c;
+}
+
 template<class T> void Polynomial<T>::display() {
 	poly.displayAll();
 	cout << endl;
@@ -226,20 +339,60 @@ Polynomial<T> Polynomial<T>::operator+(const Polynomial<T>& b) const {
 	ChainIterator<Term<T>> ai = poly.begin(), bi = b.poly.begin();
 	Polynomial<T> c;
 
+    while(ai.NotNull()){
+        c.add(ai->coef,ai->exp);
+        ai.Next();
+    }
+    while(bi.NotNull()){
+        c.add(bi->coef,bi->exp);
+        bi.Next();
+    }
+    return c;
 
 }
+
+template <class T>
+Polynomial<T>::Polynomial(const Polynomial<T> &p){
+    ChainIterator<Term<T>> li = p.poly.begin();
+
+    while(li.NotNull()){
+        add(li->coef,li->exp);
+        li.Next();
+    }
+
+}
+
+template <typename U>
+istream& operator>>(istream& os,const Polynomial<U>& p){
+    os>>p;
+    return os;
+}
+
+template <typename U>
+ostream& operator<<(ostream& os, Polynomial<U>& p){
+    os<<p.poly<<endl;
+    return os;
+}
+
+
 
 
 int main(void) {
 
-	Polynomial<int> a, b, sum;
+    //계수부분은 double 형식으로
+	Polynomial<double> a, b, sum;
 
 	char select;
-	Term<int>* tempTerm;
-	ChainNode<Term<int>> cn;
-	Chain<Term<int>> ca, cb;
-	ChainIterator<Term<int>> iter;
-	int c, e;
+	Term<double>* tempTerm;
+	ChainNode<Term<double>> cn;
+	Chain<Term<double>> ca, cb;
+	ChainIterator<Term<double>> iter;
+	double c;
+    int e; //지수는 그대로 int형식
+
+    //난수 생성
+    srand(0); //시드 0으로 초기화
+
 
 	cout << endl << "명령 선택: a: Add_a, b: Add_b, p: a + b, d: DisplayAll, q: exit" << endl;
 	cin >> select;
@@ -248,20 +401,31 @@ int main(void) {
 		switch (select)
 		{
 		case 'a':
-			cout << "리스트 a의 항 입력 : " << endl;
-			cout << "정수 coef: ";
-			cin >> c;
-			cout << "정수(내림차순) exp: ";
-			cin >> e;
+
+			cout<<"auto input c,e data"<<endl;
+            c = (rand()/static_cast<double>(RAND_MAX)*2-1)*9; //-9~9까지 double형 변수 생성
+            e = rand()%6; //0~5 사이의 난수 생성
+
+            cout<<"c : "<<c<<", e : "<<e<<endl;
+
 			a.add(c, e);//지수를 임의 숫자로 입력하여도 지수 내림 차순으로 정렬
-			break;
+
+            a.display();
+
+            break;
 		case 'b':
-			cout << "리스트 b의 항 입력: " << endl;
-			cout << "정수 coef: ";
-			cin >> c;
-			cout << "정수(내림차순) exp: ";
-			cin >> e;
-			b.add(c, e);
+
+			cout<<"auto input c,e data"<<endl;
+            c = (rand()/static_cast<double>(RAND_MAX)*2-1)*9; //-9~9까지 double형 변수 생성
+            e = rand()%6; //0~5 사이의 난수 생성
+
+            cout<<"c : "<<c<<", e : "<<e<<endl;
+
+			b.add(c, e);//지수를 임의 숫자로 입력하여도 지수 내림 차순으로 정렬
+            b.display();
+
+
+
 			break;
 		case 'p': //a+b
 			cout << "a+b: ";
